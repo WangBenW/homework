ee452921e3da5e0bc68d7067de7f7e8fe3c9be83
WangBenW <43238437+WangBenW@users.noreply.github.com>
2018-09-14 23:58:00 +0800

    Update beer.java

--- beer.java
+++ beer.java
@@ -6,17 +6,17 @@ public class beer{
 		{
                         if(i>2)
 			{
-				System.out.println(i+"bottles of beer on the wall,"+i+"bottles of beer.\nTake one down\nPass it around \n"+(i-1)+"bottles of beer on the wall\n");
+				System.out.println(i+" bottles of beer on the wall,"+i+" bottles of beer.\nTake one down\nPass it around \n"+(i-1)+" bottles of beer on the wall\n");
 			}
 
 			else if(i==2)
 			{
-				System.out.println(i+"bottles of beer on the wall,"+i+"bottles of beer.\nTake one down\nPass it around \n"+(i-1)+"bottle of beer on the wall\n");
+				System.out.println(i+" bottles of beer on the wall,"+i+" bottles of beer.\nTake one down\nPass it around \n"+(i-1)+" bottle of beer on the wall\n");
 			}
 
 			else if(i==1)
 			{
-				System.out.println(i+"bottle of beer on the wall,"+i+"bottles of beer.\nTake one down\nPass it around \n"+(i-1)+"bottle of beer on the wall\n");
+				System.out.println(i+" bottle of beer on the wall,"+i+" bottles of beer.\nTake one down\nPass it around \n"+(i-1)+" bottle of beer on the wall\n");
 			}
 	                else
 			{
