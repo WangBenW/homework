4f8b3ca063468ae1490453687b8ad6f1b0af2864
WangBenW <43238437+WangBenW@users.noreply.github.com>
2018-09-15 01:11:52 +0800

    Add files via upload

---
+++ Address.java
@@ -0,0 +1,12 @@
+public class Address{
+   public static void main(String[] args)
+   {
+       String a,c="1234";
+       a=c;
+       a="5678";
+       System.out.println(c);//结果证明c不改变
+       
+   }
+}
+
+//输出地址还没弄清楚，再看看...
