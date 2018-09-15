046416ed876b4d0676606a2b285dfb8bde202770
WangBenW <43238437+WangBenW@users.noreply.github.com>
2018-09-15 11:05:49 +0800

    Add files via upload

--- Address.java
+++ Address.java
@@ -1,12 +1,13 @@
 public class Address{
    public static void main(String[] args)
    {
-       String a,c="1234";
+       String a=new String();
+       String c=new String("1234");
        a=c;
        a="5678";
        System.out.println(c);//结果证明c不改变
-       
+
+       System.out.println(System.identityHashCode(c));//地址为1627800613
+       System.out.println(System.identityHashCode(a));//地址为2065530879
    }
 }
-
-//输出地址还没弄清楚，再看看...
